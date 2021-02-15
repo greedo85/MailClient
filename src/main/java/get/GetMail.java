cb12255f806f4ad9a66b2a78039a58202a0331e7
greedo85 <qiliveadata@gmail.com>
2021-02-15 10:18:48 +0100

    #3 przygotowanie bazy danych

--- src/main/java/get/GetMail.java
+++ src/main/java/get/GetMail.java
@@ -1,7 +1,13 @@
 package get;
+
 import javax.mail.*;
 import javax.mail.internet.MimeBodyPart;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class GetMail {
 
@@ -11,10 +17,13 @@ public class GetMail {
     private Store store;
     private Folder folder;
     private Message message;
-    private Message []messages;
-    private Address [] addresses;
+    private Message[] messages;
+    private Address[] addresses;
     private Multipart multipart;
     private MimeBodyPart mimeBodyPart;
+    private Scanner scanner;
+    private File file;
+
 
     private final String user;
     private final String password;
@@ -22,27 +31,62 @@ public class GetMail {
     public GetMail( String user, String password ) {
         this.user = user;
         this.password = password;
+        scanner = new Scanner(System.in);
     }
 
-    public void setConnection(String host, int port) throws MessagingException {
-        properties=new Properties();
+    public void setConnection( String host, int port ) throws MessagingException {
+        properties = new Properties();
         properties.put("mail.store.protocol", "imap");
         properties.put("mail.pop3.host", host);
         properties.put("mail.pop3.port", String.valueOf(port));
         properties.put("mail.pop3.starttls.enable", "true");
         session = Session.getDefaultInstance(properties);
         store = session.getStore("impas");
-        store.connect(host,user,password);
+        store.connect(host, user, password);
 
     }
 
-    public void getMessages(String folderName) throws MessagingException {
+    public void getMessages( String folderName ) throws MessagingException, IOException {
         folder = store.getFolder(folderName);
         folder.open(Folder.READ_WRITE);
         messages = folder.getMessages();
-
+        for (int i = 0; i < messages.length; i++) {
+            message = messages[i];
+            addresses = message.getFrom();
+            /*test*/
+            String from = message.getFrom().toString();
+            String subject = message.getSubject();
+            Date sendDate = message.getSentDate();
+            String messageText = message.getContent().toString();
+            String attachedFiles = "";
+            if (message.getContentType().contains("multipart")) {
+                multipart = (Multipart) message.getContent();
+                for (int j = 0; j < multipart.getCount(); j++) {
+                    mimeBodyPart = (MimeBodyPart) multipart.getBodyPart(j);
+                    if (Part.ATTACHMENT.equals(mimeBodyPart.getDisposition())) {
+                        menu();
+                        file = new File(saveDirectory + "/" + mimeBodyPart.getFileName());
+                        mimeBodyPart.saveFile(file);
+                    }
+                }
+            }
+        }
 
     }
 
+    public void menu() {
+        char choice;
+
+        choice = scanner.next().charAt(0);
+        System.out.println("zapisać załącznik?");
+        switch (choice) {
+            case 'y':
+                System.out.println("podaj nazwę katalogu: ");
+                saveDirectory = scanner.nextLine();
+                break;
+            case 'n':
+                break;
+        }
+    }
 
 }
